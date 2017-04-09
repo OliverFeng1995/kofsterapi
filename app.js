df79cf8e439eb7ed29f77c2c6b4b0d8ad97ef884
Eduardo Velloso <eduardo.velloso@unimelb.edu.au>
2017-04-10 09:03:05 +1000

    Handle post route

--- app.js
+++ app.js
@@ -5,8 +5,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
-    res.send('Hello World');
+app.post('/api',function(req,res){
+    res.send('Received post')
+});
+
+app.get('/api',function(req,res){
+    res.send('Received get');
 });
 
 app.listen(3000,function(req,res){
